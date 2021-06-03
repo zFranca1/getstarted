0e2834e7d164f98a572ae478382a9309401b647a
zFranca1 <joaopedro.franca1@hotmail.com>
2021-06-02 21:59:36 -0300

    bora fi

---
+++ config.js
@@ -0,0 +1,42 @@
+// Username displayed in greeting.
+var userName = "João pedro";
+
+// Default Searchengine
+// Available: Google DuckDuckGo Bing Yahoo
+var searchEngine = "Google";
+
+// Cards
+var cards = [{
+        name: "General",
+        bookmarks: {
+            "class schedule": "https://docs.google.com/spreadsheets/d/10D2wYVYu9sNEmXix-NnhaVZ7CiU8VvB1/edit#gid=293522039",
+            "github": "https://github.com/",
+        }
+    },
+    /*{
+        name: "Tech",
+        bookmarks: {
+
+
+
+        }
+    },*/
+    {
+        name: "Study",
+        bookmarks: {
+            "Moodle": "http://moodle.pep2.ifsp.edu.br/my/",
+            "Suap": "https://suap.ifsp.edu.br/",
+            "Drive": "https://drive.google.com/drive/u/0/my-drive",
+        }
+    },
+    {
+        name: "Social",
+        bookmarks: {
+            "youtube": "https://www.youtube.com",
+            "twitch.tv": "https://www.twitch.tv/directory/following",
+            "facebook": "https://www.facebook.com",
+            "reddit": "https://www.reddit.com",
+            "Whatsapp": "https://web.whatsapp.com/"
+        }
+    }
+]
